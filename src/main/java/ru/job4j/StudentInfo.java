713e672e2f25416ec85d91fecbe2ba27b410b10e
DOM <popov_m.v@mail.ru>
2022-06-19 21:29:20 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Mike Popov");
+        System.out.println("06.04.1986");
+
+    }
+}
