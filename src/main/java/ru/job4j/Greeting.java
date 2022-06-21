eb618718f4c160b8c26c63dcb18c8d243087b7e6
DOM <popov_m.v@mail.ru>
2022-06-21 21:01:11 +0300

    9.1. String [#237407]

--- src/main/java/ru/job4j/Greeting.java
+++ src/main/java/ru/job4j/Greeting.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!";
+        String idea = "I like Java! ";
         idea += "But I am a newbie.";
         int year = 2022;
         idea += year;
